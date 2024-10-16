5a185b4ffa5d7e0a28522dacb8f25be84dbbe473
h$.weird <dmanh20112000@gmail.com>
2024-10-16 13:10:09 +0700

    add config sqlalchemy

--- app/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models import User
-from app.core.database import engine, Base
-
-Base.metadata.create_all(bind=engine)
--- app/api/v1/auth/controller.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get('/auth')
-def view_auth():
-    return { 'message': 'Hello Auth' }
